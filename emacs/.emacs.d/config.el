1921496a66176474d90c3cb76a1691f075fc828c
Jakob  Aaes <jakob1379@gmail.com>
2019-01-18 09:48:58 +0100

    .gitignore fix

--- emacs/.emacs.d/config.el
+++ emacs/.emacs.d/config.el
@@ -80,6 +80,7 @@
 (define-globalized-minor-mode global-fci-mode fci-mode (
   lambda () (fci-mode 1)))
 (global-fci-mode 1)
+(setq fci-rule-column 80)
 
 (require 'flyspell)
 (add-hook 'LaTeX-mode-hook
