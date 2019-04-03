7e8ec43ce9e1981523667ff141f1e7f424a5fe14
Jakob <jakob1379@gmail.com>
2019-04-03 18:51:22 +0200

    Polybar finallly
    finally got polybar to work an running on manjaro!

--- polybar_manjaro/.config/polybar/polybar.sh
+++ polybar_manjaro/.config/polybar/polybar.sh
@@ -8,11 +8,11 @@ echo Found $(echo $connectedDisplays | sed 's/ /\n/g' | wc -l) display\(s\): $co
 #Take the first display to be the primary
 mainMonitor=$(echo $connectedDisplays | awk '{print $1}')
 echo Using $mainMonitor as primary display
-MONITOR=$mainMonitor polybar master &> /dev/null &
+MONITOR=$mainMonitor polybar main &> /dev/null &
 secondaryDisplays=$(echo $connectedDisplays | sed 's/ /\n/g' | tail -n +2)
 if [ ! -z "$secondaryDisplays" ]; then
     for display in $secondaryDisplays; do
-        MONITOR=$display polybar master &> /dev/null &
+        MONITOR=$display polybar main &> /dev/null &
         echo "Starting secondary polybar on $display"
     done
 fi
